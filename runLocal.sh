5361efc895ec0b67c95c201bd14de99d4d35b3e3
daanhorn <daanhorn@gmail.com>
2016-01-17 16:43:17 +0100

    run local without Docker

--- runLocal.sh
+++ runLocal.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker run -d -p 80:80 -e HUGO_BASE_URL=http://localhost daanhorn/daanhorn.nl
+#docker run -d -p 80:80 -e HUGO_BASE_URL=http://localhost daanhorn/daanhorn.nl
+hugo server --baseUrl=localhost \
+            --watch=true \
+            --source=site
