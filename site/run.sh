9c24c1fe323936d6229e716565dec10c1570b85f
daanhorn <daanhorn@gmail.com>
2016-01-17 16:46:59 +0100

    use current source folder

--- site/run.sh
+++ site/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #docker run -d -p 80:80 -e HUGO_BASE_URL=http://localhost daanhorn/daanhorn.nl
 hugo server --baseUrl=localhost \
-            --watch=true \
-            --source=site
+            --watch=true
